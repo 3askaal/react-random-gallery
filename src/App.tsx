d849dbd834266e032911b203c013fc7467eb1889
Bas Kaal <3askaal@gmail.com>
2024-03-04 03:10:51 +0100

    init project, copy logic from source project, refactor logic

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { times, random } from 'lodash';
+
+import { Gallery } from '../lib'
+
+import 'reset-css/reset.css'
+import 'normalize.css/normalize.css'
+
+const getNumber = () => Math.ceil(random(80, 250) / 10) * 10;
+const images = times(20, () => ({ url: `https://placekitten.com/${getNumber()}/${getNumber()}` }));
+
+const App = () => {
+  return (
+    <div>
+      <Gallery images={images} />
+    </div>
+  )
+};
+
+export default App
