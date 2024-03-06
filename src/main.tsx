423489026e8d6a1ee7aa117271875d9d2be5fa80
Bas Kaal <3askaal@gmail.com>
2024-03-06 20:38:59 +0100

    improve options logic

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
 
+import 'reset-css/reset.css'
+import 'normalize.css/normalize.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
